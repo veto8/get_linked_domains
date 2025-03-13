56fcb6badae66345da17ce7a663a83c4398e907f
veto <veto@myridia.com>
2025-03-13 14:30:35 +0700

    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import platform, time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import validators
 
 
 class GetDomains:
@@ -17,17 +18,28 @@ class GetDomains:
     def start(self):
         while len(self.o):
             time.sleep(0.5)
-            print("...browser: {}".format(self.o[0]))
             self.browse_page(self.o[0])
-            print("...open: {}".format(len(self.o)))
-            print("...closed: {}".format(len(self.c)))
-            print("...ext: {}".format(len(self.e)))
+            print("open: {}".format(len(self.o)))
+            print("closed: {}".format(len(self.c)))
+            print("ext: {}".format(len(self.e)))
             print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
         else:
             print("...stop")
 
     def browse_page(self, req_url):
+        print("...req:\t{}".format(self.o[0]))
+
+        if req_url in self.c:
+            if req_url in self.o:
+                self.o.remove(req_url)
+            return
+
+        if not validators.url(req_url):
+            if req_url in self.o:
+                self.o.remove(req_url)
+            return
+
         chrome_options = Options()
         chrome_options.add_argument("--headless=new")  # for Chrome >= 109
 
@@ -37,27 +49,41 @@ class GetDomains:
         items = soup.find_all("a")
 
         for i in items:
+            # extract the url of the page
             url = i.get("href")
+
+            # if the url has not starting protocol and domain, then  adding it
             if not url.startswith("https://"):
                 url = "https://" + self.domain + url
-                # print(url)
+
+            # remove ending forwardslash
             if url[-1] == "/":
                 url = url[:-1]
-            if url.startswith("https://" + self.domain):
-                if url == req_url:
-                    if url not in self.c and url not in self.o:
-                        self.c.append(url)
-                else:
-                    if url not in self.o and url not in self.c:
-                        self.o.append(url)
-            else:
-                if url not in self.e:
-                    self.e.append(url)
 
-            if req_url in self.o:
-                self.o.remove(req_url)
-                if url not in self.c:
+            if req_url == url:
+                if req_url not in self.c:
                     self.c.append(req_url)
+                if req_url in self.o:
+                    self.o.remove(req_url)
+                continue
+
+            # if this url is already recorded skip loop
+            if url in self.c or url in self.e:
+                continue
+
+            # if the url is for external, then save it to the external list
+            if url.startswith("https://" + self.domain) == False:
+                self.e.append(url)
+                continue
+
+            if url not in self.o and url not in self.c:
+                print("...add:\t{}".format(url))
+                self.o.append(url)
+
+        if req_url in self.o:
+            print("...rm:\t{}".format(req_url))
+            self.o.remove(req_url)
+            self.c.append(req_url)
 
 
 if __name__ == "__main__":
