dc1ee98ab42596c7d1eb61d9a6ee16af2f98da53
veto <veto@lab.local>
2025-03-16 17:43:03 +0700

    addGraph

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import validators
 import multiprocessing as mp
+import networkx as nx
+from pyvis.network import Network
 
 class GetDomains:
     def __init__(self, domain="127.0.0.1", proc=3):
@@ -117,12 +119,36 @@ class GetDomains:
 
     def complete(self):
         print("...completed!");
-        print(self.c);        
+        print(self.c);
+
+    def graph(self):
+        print("...graph");
+        net  = Network()
+        #Create directed graph object
+        Graph = nx.DiGraph()
+        #Create nodes
+        Graph.add_node("C++")
+        Graph.add_node("Python")
+        Graph.add_node("Java")
+        Graph.add_node("C#")
+        Graph.add_node("Go")
+        Graph.add_node("Julia")
+        #For adding the edges between nodes with weight and value (Edges with higher value will appear as bold)
+        Graph.add_edge("C++", "C#", weight=.75, value=20)
+        Graph.add_edge("Python", "Julia", weight=.75, value=20)
+        Graph.add_edge("Java", "Go", weight=.5) 
+        Graph.add_edge("Python", "C#", weight=.5)
+        Graph.add_edge("Go", "C++", weight=.5)
+        Graph.add_edge("Java", "Julia", weight=.5)
+        Graph.add_edge("Java", "C#", weight=.75, value=20)
+        #For visualizing the graph
+        net.from_nx(Graph)
+        net.show("graph.html",notebook=False)
 
 if __name__ == "__main__":
     d = GetDomains("127.0.0.1", 6)
-    d.start()
-    d.complete()    
-    #d.request_page("http://127.0.0.1")
+    #d.start()
+    #d.complete()    
+    d.graph()
 
 
