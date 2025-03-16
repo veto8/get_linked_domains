3260215f246b771400e445c08081dc4a1a044876
veto <veto@lab.local>
2025-03-17 01:06:57 +0700

    addrun

--- README.md
+++ README.md
@@ -9,5 +9,17 @@ python3 -m venv env
 . env/bin/activate
 pip install pip --upgrade
 pip install -r requirements.txt
+```
+
+## Test run
+### Run Docker to simulate a nested website on local host 127.0.0.1
+```
+cd website/dockers/
+docker-compose up -d
+```
+
+### Run ./main.py to access the docker website what runs on your local host 127.0.0.1
+```
+cd ../../
 ./main.py
 ```
--- main.py
+++ main.py
@@ -8,16 +8,17 @@ import validators
 import multiprocessing as mp
 import networkx as nx
 from pyvis.network import Network
+import time,datetime
+
 
 class GetDomains:
     def __init__(self, domain="127.0.0.1", proc=3):
+        self.init_time = round(time.time())
         self.domain = domain
         self.p = proc
         self.o = []
         self.o.append("http://" + domain)
         self.graph = nx.DiGraph()
-        #for i in range(1,100):
-        #  self.o.append("http://{0}/sku/{1}.html".format(domain,i))
         self.c = []
         self.e = []
 
@@ -41,68 +42,57 @@ class GetDomains:
                   target=self.get_page_items,
                   args=(
                       v,
-                      k,
                       return_dict
                    ),
                  )
                 jobs.append(p)
                 p.start()                
               #print("...tread.................")
-              print("proc: {0} \t open: {1} \t closed: {2} \t ext: {3}".format(self.p,len(self.o),len(self.c),len(self.e)))                        
+              print("proc: {0}\t open:{1}\t closed:{2}\t ext:{3}\t time passed: {4}".format(self.p,len(self.o),len(self.c),len(self.e),datetime.timedelta(seconds=round(time.time()) - self.init_time)))    
               
               for proc in jobs:
                 proc.join()
                 
-              for k,v in enumerate(return_dict.values()):
-                 #print(k)
-                 self.process_items(v)                  
+              for k,v in enumerate(return_dict):
+                 #print(v)
+                 #print(return_dict[v])                  
+                 self.graph.add_node(v)
+                 for ii in return_dict[v]:
+                   self.graph.add_node(ii)
+                   self.graph.add_edge(v,ii,weight=.5, value=20)
+                 self.process_items(return_dict[v])                  
+            #break
 
 
 
-
-    def get_page_items(self,req_url, id, return_dict ):
-        if validators.url(req_url):
-          #print("...req:\t{}".format(req_url))
-          chrome_options = Options()
-          chrome_options.add_argument("--headless=new")  # for Chrome >= 109
-
-          browser = webdriver.Chrome(options=chrome_options)
-          browser.get(req_url)
-          soup = BeautifulSoup(browser.page_source, "html.parser")
-          _items = soup.find_all("a")
-          items = []
-          for i in _items:
-            items.append(i.get("href"))          
-          #print(items)          
-          return_dict[id] = items
+    def get_page_items(self,req_url, return_dict ):
+      _items = self.request_page(req_url)
+      items = []
+      if len(_items):
+        for i in _items:
+          if not i.startswith("http://"):
+            i = "http://{0}{1}".format(self.domain , i)
+          if i[-1] == "/":
+            i = i[:-1]            
+          items.append(i)
+        return_dict[req_url] = items
 
     def request_page(self,req_url ):
+        items = []        
         if validators.url(req_url):
-          #print("...req:\t{}".format(req_url))
           chrome_options = Options()
           chrome_options.add_argument("--headless=new")  # for Chrome >= 109
           browser = webdriver.Chrome(options=chrome_options)
           browser.get(req_url)
           soup = BeautifulSoup(browser.page_source, "html.parser")
           _items = soup.find_all("a")
-          items = []
           for i in _items:
             items.append(i.get("href"))
-          print(items)
+        return items
 
     def process_items(self, items):
         for url in items:
-            # extract the url of the page
-
-            # if the url has not starting protocol and domain, then  adding it
-            if not url.startswith("http://"):
-                url = "http://" + self.domain + url
-
-            # remove ending forwardslash
-            if url[-1] == "/":
-                url = url[:-1]
-
-
+            
             # if this url is already recorded skip loop
             if url in self.c or url in self.e:
                 continue
@@ -120,36 +110,15 @@ class GetDomains:
 
     def complete(self):
         print("...completed!");
-        print(self.c);
-
-    def graph(self):
-        print("...graph");
-        net  = Network()
-        #Create directed graph object
-        Graph = nx.DiGraph()
-        #Create nodes
-        Graph.add_node("C++")
-        Graph.add_node("Python")
-        Graph.add_node("Java")
-        Graph.add_node("C#")
-        Graph.add_node("Go")
-        Graph.add_node("Julia")
-        #For adding the edges between nodes with weight and value (Edges with higher value will appear as bold)
-        Graph.add_edge("C++", "C#", weight=.75, value=20)
-        Graph.add_edge("Python", "Julia", weight=.75, value=20)
-        Graph.add_edge("Java", "Go", weight=.5) 
-        Graph.add_edge("Python", "C#", weight=.5)
-        Graph.add_edge("Go", "C++", weight=.5)
-        Graph.add_edge("Java", "Julia", weight=.5)
-        Graph.add_edge("Java", "C#", weight=.75, value=20)
-        #For visualizing the graph
-        net.from_nx(Graph)
-        net.show("graph.html",notebook=False)
+        net  = Network()        
+        net.from_nx(self.graph)
+        net.show("graph.html",notebook=False)        
+        #print(self.c);
 
 if __name__ == "__main__":
-    d = GetDomains("127.0.0.1", 6)
+    d = GetDomains("127.0.0.1", 8)
     d.start()
     d.complete()    
-    #d.graph()
+
 
 
